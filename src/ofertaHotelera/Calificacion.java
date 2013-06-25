3400ef3c53975dbc972c794d50396350a764532b
dimegliofernando <dimegliofernando@gmail.com>
2013-06-25 20:22:54 +0000

    clase calificacion

---
+++ src/ofertaHotelera/Calificacion.java
@@ -0,0 +1,37 @@
+package ofertaHotelera;
+
+public class Calificacion {
+	
+	private Usuario user;
+	private int puntaje;
+	private String comentario;
+	
+	public Calificacion(Usuario usuario, int punt, String comment){
+		
+		setUser(usuario);
+		setPuntaje(punt);
+		setComentario(comment);
+	}
+	
+	public Usuario getUser() {
+		return user;
+	}
+	public void setUser(Usuario user) {
+		this.user = user;
+	}
+	public int getPuntaje() {
+		return puntaje;
+	}
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	
+
+}
